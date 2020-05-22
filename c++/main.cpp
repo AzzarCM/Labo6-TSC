20ec20e8c3fe11d09f5de190a9a34c86d8d69211
Cristian Mundo <00086917@uca.edu.sv>
2020-05-21 22:04:29 -0600

    just updating my carne

--- c++/main.cpp
+++ c++/main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
     Vector T;
 
     cout << "IMPLEMENTACI"<<char(224)<<"N DEL M"<<char(144)<<"TODO DE LOS ELEMENTOS FINITOS\n"
-         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 2 DIMENSIONES\n"
-         << "\t- FUNCIONES DE FORMA LINEALES\n" << "\t- PESOS DE GALERKIN\n"
+         << "\tEN 2 DIMENSIONES\n"
+         << "\tCON FUNCIONES DE FORMA LINEALES\n" << "\tY PESOS DE GALERKIN - 00086917\n"
          << "*********************************************************************************\n\n";
 
     mesh m;
